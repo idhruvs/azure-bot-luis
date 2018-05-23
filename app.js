078ca0be7efccae3463f2f47b2723a30994ad187
Dhruv Shah <dhruv.shah4192@gmail.com>
2018-05-23 20:33:23 +0530

    added config and updated token url

--- app.js
+++ app.js
@@ -5,13 +5,47 @@ A simple Language Understanding (LUIS) bot for the Microsoft Bot Framework.
 var restify = require('restify');
 var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
+var fetch = require('node-fetch');
+var config= require('./config.js');
 
+var accessToken = '';
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.port || process.env.PORT || 3978, '127.0.0.1' ,function () {
+    console.log('%s listening to %s', server.name, server.url); 
+    _initiateTokenFetch();
 });
   
+_initiateTokenFetch = async () => {
+    console.log('Initiating Token Fetch Action');
+    try {
+        const reqHeaders = {
+            "Authorization" : "Basic c1FwRWRBTjIyZ01neXo4VWMwVXhta0VlOUF3emJJa3E6QXB6RW1SMTFQcmJhMEtWcw==",
+            "Content-Type" : "application/x-www-form-urlencoded"
+        };
+        const reqBody = {
+            "grant_type": "client_credentials"
+        }
+        console.log(reqHeaders);
+        console.log(JSON.stringify(reqBody));
+        const tokenResponse = await fetch(config.TOKEN_URL, {
+            method: 'POST',
+            headers: reqHeaders,
+            body: JSON.stringify(reqBody)
+        });
+    
+        const responseBody = await tokenResponse.json();
+        console.log(responseBody);
+        accessToken = responseBody['access_token'];
+        console.log(accessToken);
+    } catch(e) {
+        console.log(e);
+    }
+    
+    
+}
+
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
@@ -56,7 +90,7 @@ bot.recognizer(recognizer);
 // See https://docs.microsoft.com/en-us/bot-framework/nodejs/bot-builder-nodejs-recognize-intent-luis 
 bot.dialog('GreetingDialog',
     (session) => {
-        session.send('You reached the Greeting intent. You said \'%s\'.', session.message.text);
+        session.send('You reached the Greeting intent. You said \'%s\'.', accessToken);
         session.endDialog();
     }
 ).triggerAction({
---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    'TOKEN_URL' : 'https://ybsg-nonprod-dev.apigee.net/mortgage/v1.0/applications/token',
+    'PROPERTY_URL' : 'https://ybsg-nonprod-dev.apigee.net/mortgage/v1.0/applications/branchLocator?postcode=London'
+}
+
+module.exports = config;
+
