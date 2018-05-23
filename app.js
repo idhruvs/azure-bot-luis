1784c15fc0e065245e3fa8314a4450c2f2e6dff3
Dhruv Shah <dhruv.shah4192@gmail.com>
2018-05-23 19:14:41 +0530

    removed help intent from bot

--- app.js
+++ app.js
@@ -72,15 +72,6 @@ bot.dialog('Branch-Locater',
     matches: 'Branch-Locater'
 })
 
-bot.dialog('HelpDialog',
-    (session) => {
-        session.send('You reached the Help intent. You said \'%s\'.', session.message.text);
-        session.endDialog();
-    }
-).triggerAction({
-    matches: 'Help'
-})
-
 bot.dialog('CancelDialog',
     (session) => {
         session.send('You reached the Cancel intent. You said \'%s\'.', session.message.text);
